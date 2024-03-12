d04bb1a4d38f6c8e151a14d5e8afe5d205849645
alvaro_cabanillas <alvaro.cabanillas@enghouse.com>
2024-03-12 19:17:40 +0100

    cambios 1

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
-  <span>Revisar la consola de JavaScript</span>
+  <span>Revisada la consola de JavaScript</span>
 `
 
